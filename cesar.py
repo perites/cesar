41ffcb6cc32d859e84833637401a9fab38b66407
perites <111568507+perites@users.noreply.github.com>
2022-09-25 19:33:29 +0300

    Update cesar.py

--- cesar.py
+++ cesar.py
@@ -5,13 +5,18 @@ letter_l = list(string.ascii_lowercase)
 letter_u = list(string.ascii_uppercase)
 nletters_l = 	{}
 nletters_u = {}
-new_word = []
 
 
 
-def encryption(word , new_word):
+
+def encryption(word,step):
+	for x in range(0, len(letter_l)):
+		nletters_l[letter_l[x]] = letter_l[x+step-len(letter_l)]
+	for x in range(0, len(letter_u)):
+		nletters_u[letter_u[x]] = letter_u[x+step-len(letter_u)]
 
 	word = list(word)
+	new_word = []
 	for l in word:
 		if l != " ":
 			if l in letter_l:
@@ -20,7 +25,6 @@ def encryption(word , new_word):
 				new_word.append(nletters_u[l])
 		else:
 			new_word.append(" ")
-
 	new_word = "".join(new_word)
 	return new_word
 
@@ -29,19 +33,14 @@ def encryption(word , new_word):
 
 
 
+
+print(encryption("aaa" , 3) == "ddd")
+print(encryption("ZZZ" , 3) == "CCC")
+print(encryption("bBb" , 3) == "eEe")
+print(encryption("CcC" , 3) == "FfF")
+
+
 step = int(input("step : "))
 word = input("word : ")
-
-
-
-for x in range(0, len(letter_l)):
-	nletters_l[letter_l[x]] = letter_l[x+step-len(letter_l)]
-for x in range(0, len(letter_u)):
-	nletters_u[letter_u[x]] = letter_u[x+step-len(letter_u)]
-
-
-
-print(encryption(word, new_word))
-
-
+print(encryption(word))
 input()
